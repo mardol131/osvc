32a786d4e03fc6baea2cf1ba4c6a2840b69ded3a
MrHyaena <dolezalmartin131@gmail.com>
2025-07-07 22:06:43 +0200

    Frontend: TextSection copy styling change

--- frontend/app/_components/homepage/TextSection.tsx
+++ frontend/app/_components/homepage/TextSection.tsx
@@ -6,8 +6,10 @@ export default function TextSection() {
       id="benefits"
       className=" flex items-center justify-center md:px-10 px-4 py-10 bg-primary text-textLight"
     >
-      <div className="max-w-wrapper flex flex-col gap-10 items-center text-center">
-        <h3>Je to jako pojištění proti státní neschopnosti</h3>
+      <div className="max-w-wrapper flex flex-col gap-10 items-center justify-center text-center mb-[-4px]">
+        <p className="text-xl font-bebas uppercase">
+          Je to jako pojištění proti státní neschopnosti
+        </p>
       </div>
     </div>
   );
